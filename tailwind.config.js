03c647f7a6ab9ab311017acc4d0f6b1bdd4b23c8
Mitsu <tech.mitsu.yuki@gmail.com>
2023-11-14 22:38:55 +0900

    Add: content for html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/.vitepress/**/*.{js,ts,vue}'
+    './pages/.vitepress/**/*.{html,js,ts,vue}',
   ],
   theme: {
     extend: {},
